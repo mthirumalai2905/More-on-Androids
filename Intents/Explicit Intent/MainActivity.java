6e5773794b3520ca9eeb58c817475566d38b4bd4
Thirumalai <98790479+mthirumalai2905@users.noreply.github.com>
2023-04-13 11:48:46 +0530

    Create MainActivity.java

---
+++ Intents/Explicit Intent/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.explicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn1;
+    TextView txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txt = findViewById(R.id.textView);
+        btn1 = findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Explicit Intent
+                Intent i = new Intent(getApplicationContext(), Activity3.class);
+
+                startActivity(i);
+            }
+        });
+    }
+}
